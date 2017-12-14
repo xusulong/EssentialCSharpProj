1c68e5c9904ca4610ef4210ea3f31793d1b4a291
城北徐公 <isulong@outlook.com>
2017-12-14 11:15:12 +0800

    完成第18章

---
+++ Chapter18/Listing18.25.ParallelPiCalculation.cs
@@ -0,0 +1,24 @@
+﻿namespace EssentialCSharp.Chapter18.Listing18_25
+{
+    using System;
+    using System.Threading.Tasks;
+    using EssentialCSharp.Shared;
+    class Program
+    {
+        const int TotalDigits = 100;
+        const int BatchSize = 10;
+        public static void Main()
+        {
+            string pi = null;
+            const int iterations = TotalDigits / BatchSize;
+            string[] sections = new string[iterations];
+            //TPL提供了一个辅助方法，并行迭代
+            Parallel.For(0,iterations,(i)=>
+            {
+                sections[i] = PiCalculator.Calculate(BatchSize, i * BatchSize);
+            });
+            pi = string.Join("",sections);
+            Console.WriteLine(pi);
+        }
+    }
+}
---
+++ Chapter18/Listing18.29.LinqSelect.cs
@@ -0,0 +1,31 @@
+﻿namespace EssentialCSharp.Chapter18.Listing18_29
+{
+    using EssentialCSharp.Shared;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class Program
+    {
+        //非并行Linq
+        public List<string> Encrypt(IEnumerable<string> data)
+        {
+            return data.Select(item => Encrypt(item)).ToList();
+        }
+        //并行Linq
+        public List<string> Encrypt2(IEnumerable<string> data)
+        {
+            return data.AsParallel().Select(
+                 item => Encrypt(item)).ToList();
+
+        }
+
+        private string Encrypt(string item)
+        {
+            Console.WriteLine($">>>>>Encrypting '{ item }'.");
+            Cryptographer cryptographer = new Cryptographer();
+            string itemEncrypted = cryptographer.Encrypt(item);
+            Console.WriteLine($"<<<<<Finished encrypting '{ itemEncrypted }'.");
+            return itemEncrypted;
+        }
+    }
+}
